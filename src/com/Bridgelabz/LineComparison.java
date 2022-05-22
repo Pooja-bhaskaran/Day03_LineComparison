4b3a0a65cd450999751631cda0b67c17d4a22013
unknown <https://github.com/Pooja-bhaskaran/Day03.git>
2022-05-22 16:54:01 +0530

    Length of the line

--- src/com/Bridgelabz/LineComparison.java
+++ src/com/Bridgelabz/LineComparison.java
@@ -1,7 +1,13 @@
 package com.Bridgelabz;
 
+import static java.lang.Math.sqrt;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to LineComparison Computation");
+        int x1 = 2, x2 =5, y1=2, y2=4;
+        double lengthOfPoints ;
+        lengthOfPoints = Math.sqrt(x2-x1) * (y2-y1);
+        System.out.println("Length of the line is : " + lengthOfPoints);
     }
 }
